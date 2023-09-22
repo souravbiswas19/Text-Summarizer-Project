e2744873220c9859feb6ced83c8b937e6f9ea1b5
Sourav Biswas <78258134+souravbiswas19@users.noreply.github.com>
2023-09-23 00:10:52 +0530

    requirement and installation done

--- setup.py
+++ setup.py
@@ -3,25 +3,28 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__="0.0.0"
 
-REPO_NAME= "Text-Summarizer-Project"
-AUTHOR_USER_NAME= "souravbiswas19"
-SRC_REPO= "textSummarizer"
-AUTHOR_EMAIL= "souravbiswas19@gmail.com"
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "souravbiswas19"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "souravbiswas19april@gmail.com"
+
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A text summarizer project",
+    description="A small python package for NLP app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"","src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
